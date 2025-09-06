5fb41d10930ad785734e16361d22161077a14eef
polyakov-artem <polyakov.contact@yandex.ru>
2025-09-06 16:51:57 +0300

    feat: add material ui, fonts, material Icons

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,9 +1,11 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './app';
+import { CssBaseline } from '@mui/material';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <CssBaseline />
     <App />
   </StrictMode>
 );
