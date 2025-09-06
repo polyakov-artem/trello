ea1e141b25738b72dff427c6169ebd0c1c19215f
polyakov-artem <polyakov.contact@yandex.ru>
2025-09-06 22:30:28 +0300

    feat: add tailwindcss

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './app';
 import { CssBaseline } from '@mui/material';
+import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { visualizer } from 'rollup-plugin-visualizer';
 import path from 'path';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), visualizer({ open: true })],
+  plugins: [react(), visualizer({ open: true }), tailwindcss()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
