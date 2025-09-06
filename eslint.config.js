dd8f088c89f50037c23b7dd9e120d3c39ea7fda3
polyakov-artem <polyakov.contact@yandex.ru>
2025-09-06 14:17:52 +0300

    build: replace deprecated tseslint.config with defineConfig

--- eslint.config.js
+++ eslint.config.js
@@ -2,22 +2,23 @@ import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
-import { globalIgnores } from "eslint/config";
 import reactX from "eslint-plugin-react-x";
 import reactDom from "eslint-plugin-react-dom";
 
-export default tseslint.config([
-  globalIgnores(["dist"]),
+import { defineConfig } from "eslint/config";
+
+export default defineConfig(
+  {
+    ignores: ["**/build/**", "**/dist/**"],
+  },
+  tseslint.configs.recommended,
+  tseslint.configs.recommendedTypeChecked,
+  reactHooks.configs["recommended-latest"],
+  reactRefresh.configs.vite,
+  reactX.configs["recommended-typescript"],
+  reactDom.configs.recommended,
   {
     files: ["**/*.{ts,tsx}"],
-    extends: [
-      tseslint.configs.recommended,
-      ...tseslint.configs.recommendedTypeChecked,
-      reactHooks.configs["recommended-latest"],
-      reactRefresh.configs.vite,
-      reactX.configs["recommended-typescript"],
-      reactDom.configs.recommended,
-    ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
@@ -26,5 +27,5 @@ export default tseslint.config([
         tsconfigRootDir: import.meta.dirname,
       },
     },
-  },
-]);
+  }
+);
