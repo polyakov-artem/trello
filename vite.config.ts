5cefb68dfb3d64f4250075e30ddf92d5bd0e2c56
polyakov-artem <polyakov.contact@yandex.ru>
2025-09-08 14:56:15 +0300

    build: add PUBLIC_PATH variable to vite.config.ts, add feature to deploy project

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,20 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import { visualizer } from 'rollup-plugin-visualizer';
 import path from 'path';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), visualizer({ open: true })],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  const PUBLIC_PATH = env.VITE_PUBLIC_PATH || '/';
+
+  return {
+    plugins: [react(), visualizer({ open: true })],
+    base: PUBLIC_PATH,
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
     },
-  },
+  };
 });
