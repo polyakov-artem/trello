151a74eba45936a8139e2ab538d7781c5289eb3b
polyakov-artem <polyakov.contact@yandex.ru>
2025-09-10 16:22:05 +0300

    fix: change userApi, usersStore

--- src/entities/user/model/usersStore.ts
+++ src/entities/user/model/usersStore.ts
@@ -11,25 +11,54 @@ export type UserWithoutId = Omit<User, 'id'>;
 
 export type UsersStoreState = {
   users: User[];
+  isLoadingUsers: boolean;
+  usersLoadingError: string;
+  isRemovingUser: boolean;
+  userRemovingError: string;
+  isAddingUser: boolean;
+  userAddingError: string;
   getUsers: () => User[];
+  setUsers: (users: User[]) => void;
   removeUser: (id: string) => void;
-  addUser: (user: UserWithoutId) => void;
+  setIsLoadingUsers: (isLoading: boolean) => void;
+  setIsRemovingUser: (isRemovingUser: boolean) => void;
+  setIsAddingUser: (isAddingUser: boolean) => void;
+  setUserRemovingError: (error: string) => void;
+  setUserAddingError: (error: string) => void;
+  setUsersLoadingError: (error: string) => void;
+  addUser: (user: User) => void;
 };
 
 export const useUsersStore = create<UsersStoreState>((set, get) => ({
   users: [],
+  isLoadingUsers: false,
+  isRemovingUser: false,
+  isAddingUser: false,
+  usersLoadingError: '',
+  userRemovingError: '',
+  userAddingError: '',
   getUsers: () => get().users,
+  setUsers: (users: User[]) => {
+    set({ users });
+  },
   removeUser: (id: string) => {
     set({ users: get().users.filter((u) => u.id !== id) });
   },
   addUser: (user) => {
-    const users = get().users;
-
-    users.push({
-      ...user,
-      id: nanoid(),
-    });
-
-    set({ users });
+    set((state) => ({
+      users: [
+        ...state.users,
+        {
+          ...user,
+          id: nanoid(),
+        },
+      ],
+    }));
   },
+  setIsLoadingUsers: (isLoadingUsers: boolean) => set({ isLoadingUsers }),
+  setIsRemovingUser: (isRemovingUser: boolean) => set({ isRemovingUser }),
+  setIsAddingUser: (isAddingUser: boolean) => set({ isAddingUser }),
+  setUserRemovingError: (userRemovingError: string) => set({ userRemovingError }),
+  setUserAddingError: (userAddingError: string) => set({ userAddingError }),
+  setUsersLoadingError: (usersLoadingError) => set({ usersLoadingError }),
 }));
--- src/shared/api/user/userApi.ts
+++ src/shared/api/user/userApi.ts
@@ -1,4 +1,5 @@
 import { persistStorage } from '@/shared/lib/persistStorage';
+import { nanoid } from 'nanoid';
 
 export type User = {
   id: string;
@@ -6,16 +7,30 @@ export type User = {
   avatarId: string;
 };
 
+export type UserWithoutId = Omit<User, 'id'>;
+
 const USERS_STORAGE_KEY = 'trello_users';
+const ERROR_USERS_LOADING_FAILED = 'Failed to load users';
+const ERROR_USER_ADDING_FAILED = 'Failed to add user';
+const ERROR_USER_REMOVING_FAILED = 'Failed to remove user';
 
 export const userApi = {
-  async addUser(user: User): Promise<void> {
+  async addUser(user: UserWithoutId, throwError?: boolean): Promise<User> {
+    if (throwError) {
+      throw new Error(ERROR_USER_ADDING_FAILED);
+    }
     const users = await userApi.getUsers();
-    users.push(user);
+    const newUser = { ...user, id: nanoid() };
+    users.push(newUser);
     await persistStorage.setItem(USERS_STORAGE_KEY, users);
+    return newUser;
   },
 
-  async removeUser(id: string): Promise<void> {
+  async removeUser(id: string, throwError?: boolean): Promise<void> {
+    if (throwError) {
+      throw new Error(ERROR_USER_REMOVING_FAILED);
+    }
+
     const users = await userApi.getUsers();
 
     await persistStorage.setItem(
@@ -24,7 +39,11 @@ export const userApi = {
     );
   },
 
-  async getUsers(): Promise<User[]> {
+  async getUsers(throwError?: boolean): Promise<User[]> {
+    if (throwError) {
+      throw new Error(ERROR_USERS_LOADING_FAILED);
+    }
+
     return persistStorage.getItemSafe<User[]>(USERS_STORAGE_KEY, []);
   },
 };
