bdc13868b365d7686ea3135bed0ec70ce7afd314
polyakov-artem <polyakov.contact@yandex.ru>
2025-09-13 21:21:08 +0300

    feat: add react-toastify

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,8 +1,26 @@
 import type { FC } from 'react';
-import { AppRouter } from './appRouter';
+import { AppRouter } from './AppRouter';
+import { ToastContainer, Slide } from 'react-toastify';
 
 const App: FC = () => {
-  return <AppRouter />;
+  return (
+    <>
+      <AppRouter />
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+        transition={Slide}
+      />
+    </>
+  );
 };
 
 export default App;
