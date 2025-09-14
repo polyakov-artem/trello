9833dcbddb0395919113a972dd7bf5e0a129d196
polyakov-artem <polyakov.contact@yandex.ru>
2025-09-14 16:37:34 +0300

    fix: replace mui with antd

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,12 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { CssBaseline } from '@mui/material';
 import './index.css';
 import App from './app';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <CssBaseline />
     <App />
   </StrictMode>
 );
