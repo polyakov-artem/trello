e296b3765504f87fd85faec22cbcf30672dd19c3
polyakov-artem <polyakov.contact@yandex.ru>
2025-09-18 20:38:23 +0300

    feat: add @ant-design/v5-patch-for-react-19

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,7 +1,8 @@
+import './index.css';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
 import App from './app';
+import '@ant-design/v5-patch-for-react-19';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
