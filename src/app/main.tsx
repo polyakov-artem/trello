433ebef7df2315d2b633c3782cf016f36180a03d
polyakov-artem <polyakov.contact@yandex.ru>
2025-10-29 11:35:17 +0300

    feat: add ErrorBoundary

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -3,9 +3,12 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './app';
 import '@ant-design/v5-patch-for-react-19';
+import { ErrorBoundary } from '@/shared/ui/ErrorBoundary/ErrorBoundary';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
   </StrictMode>
 );
---
+++ src/shared/ui/ErrorBoundary/ErrorBoundary.tsx
@@ -0,0 +1,49 @@
+import type { PropsWithChildren, ReactNode } from 'react';
+import { Component, type ErrorInfo } from 'react';
+import { errorAdvices, errors } from '@/shared/constants/errorMsgs';
+import { ErrorWithReloadBtn } from '../ErrorWithReload/ErrorWithReloadBtn';
+
+export type ErrorBoundaryProps = PropsWithChildren & {
+  fallback?: ReactNode;
+  onError?: (error: Error, errorInfo: ErrorInfo) => void;
+};
+
+export type ErrorBoundaryState = {
+  hasError: boolean;
+  error: Error | undefined;
+};
+
+export class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props);
+    this.state = { hasError: false, error: undefined };
+  }
+
+  static getDerivedStateFromError(error: Error): ErrorBoundaryState {
+    return { hasError: true, error };
+  }
+
+  override componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    console.error('ErrorBoundary caught an error:', error, errorInfo);
+    this.props.onError?.(error, errorInfo);
+  }
+
+  override render() {
+    if (this.state.hasError) {
+      if (this.props.fallback) {
+        return this.props.fallback;
+      }
+
+      return (
+        <main className="flex flex-col min-h-dvh">
+          <ErrorWithReloadBtn
+            title={this.state.error?.message || errors.unexpected}
+            subtitle={errorAdvices.tryAgain}
+          />
+        </main>
+      );
+    }
+
+    return this.props.children;
+  }
+}
